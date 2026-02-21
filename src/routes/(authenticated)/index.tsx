1f44c07fbfb34c25f0e2b7e6004aee5e0dc33bb2
Esteban D. Barboza Primera <dario0117@gmail.com>
2026-02-21 08:49:49 -0500

    updated default org selection

--- src/routes/(authenticated)/index.tsx
+++ src/routes/(authenticated)/index.tsx
@@ -1,19 +1,29 @@
+import { logWarning } from '@lib/logger.utils';
 import { useUserOrganizationsSuspendedQuery } from '@services/organizations/list-user-organizations.http-service';
 import { createFileRoute, Navigate } from '@tanstack/react-router';
 
 function RedirectToOrganization() {
   const { data } = useUserOrganizationsSuspendedQuery();
   const organizations = data.responseData?.results ?? [];
-  const firstOrganization = organizations[0];
+  const explicitDefault = organizations.find((org) => org.isDefault);
 
-  if (!firstOrganization) {
+  if (!explicitDefault && organizations.length > 0) {
+    logWarning(
+      { organizationCount: organizations.length },
+      'No organization marked as default, falling back to first in list',
+    );
+  }
+
+  const defaultOrganization = explicitDefault ?? organizations[0];
+
+  if (!defaultOrganization) {
     return null;
   }
 
   return (
     <Navigate
       to="/$organizationSlug"
-      params={{ organizationSlug: firstOrganization.slug }}
+      params={{ organizationSlug: defaultOrganization.slug }}
     />
   );
 }
